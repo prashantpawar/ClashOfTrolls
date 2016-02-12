2543d51628521e512e9e916dc36e3a2654fd156d
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-02-12 17:39:35 -0500

    Added v1 of ClashOfTrolls contract

---
+++ app/contracts/ClashOfTrolls.sol
@@ -0,0 +1,29 @@
+import "dev.oraclize.it/api.sol";
+
+contract ClashOfTrolls is usingOraclize {
+    uint totalFunds;
+    struct Troll {
+        address addr;
+        string post_id;
+        uint reward;
+    }
+
+    mapping (uint => Troll) trolls;
+    uint numTrolls;
+
+    function depositFunds() {
+        totalFunds += msg.value;
+    }
+
+    function claimReward(string post_id) return (bool accepted) {
+        uint trollID = numTrolls++;
+        trolls[trollID] = Troll({
+            addr: msg.sender,
+            post_id: post_id,
+            reward: totalFunds/10
+        });
+        totalFunds = totalFunds - trolls[trollID].reward;
+        trolls[trollID].addr.send(trolls[trollID].reward);
+        return true;
+    }
+}
