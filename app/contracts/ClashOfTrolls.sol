78df57a94f83f38e447a26f937426004ad481055
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-02-17 23:36:04 -0500

    Added callback adn Oraclize_query for a hardcoded Reddit thread

--- app/contracts/ClashOfTrolls.sol
+++ app/contracts/ClashOfTrolls.sol
@@ -1,16 +1,18 @@
 import "dev.oraclize.it/api.sol";
 
 contract ClashOfTrolls is usingOraclize {
+    //Place where we store the total funds available for reward
     uint totalFunds;
+    
     struct Troll {
         address addr;
         string post_id;
         uint reward;
     }
 
-    mapping (uint => Troll) trolls;
-    uint numTrolls;
+    mapping (bytes32 => Troll) trolls;
 
+    //This method is used by people to deposit funds for the reward
     function depositFunds() {
         totalFunds += msg.value;
     }
@@ -23,15 +25,23 @@ contract ClashOfTrolls is usingOraclize {
         rewardSize = totalFunds/10;
     }
 
+    function __callback(bytes32 myid, string result) {
+        if (msg.sender != oraclize_cbAddress()) throw;
+        if (trolls[myid].reward > 0) {
+            trolls[myid].reward = totalFunds/10;
+            totalFunds = totalFunds - trolls[myid].reward;
+            trolls[myid].addr.send(totalFunds/10);
+        }
+    }
+    
+
     function claimReward(string post_id) returns (bool accepted) {
-        uint trollID = numTrolls++;
+        bytes32 trollID = oraclize_query("URL", "json(http://www.reddit.com/by_id/t3_469rig.json).data.children[0].data.score");
         trolls[trollID] = Troll({
             addr: msg.sender,
             post_id: post_id,
-            reward: totalFunds/10
+            reward: 0
         });
-        totalFunds = totalFunds - trolls[trollID].reward;
-        trolls[trollID].addr.send(trolls[trollID].reward);
-        return true;
+        accepted = true;
     }
 }
