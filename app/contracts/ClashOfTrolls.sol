29688d01a78947571c58938d962cdb6239b2f1ca
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-02-20 23:18:48 -0500

    Added index for post_id and proof callbacks

--- app/contracts/ClashOfTrolls.sol
+++ app/contracts/ClashOfTrolls.sol
@@ -3,7 +3,7 @@ import "dev.oraclize.it/api.sol";
 contract ClashOfTrolls is usingOraclize {
     //Place where we store the total funds available for reward
     uint totalFunds;
-    
+
     struct Troll {
         address addr; //We want to know who the troll is
         string troll_post_id; //The post on Reddit where the troll attempt was made
@@ -11,7 +11,9 @@ contract ClashOfTrolls is usingOraclize {
         uint reward; //The reward given to the troll
     }
 
-    mapping (bytes32 => Troll) trolls;
+    mapping (string => Troll) trolls;
+    mapping (bytes32 => string) post_cb_index;
+    mapping (bytes32 => string) proof_cb_index;
 
     //This method is used by people to deposit funds for the reward
     function depositFunds() {
@@ -21,23 +23,35 @@ contract ClashOfTrolls is usingOraclize {
     //This callback is used by Oraclize to inform us of the response from Reddit API.
     function __callback(bytes32 id, string result) {
         if (msg.sender != oraclize_cbAddress()) throw;
-        if (trolls[id].reward > 0) {
-            trolls[id].reward = totalFunds/10;
-            totalFunds = totalFunds - trolls[id].reward;
-            trolls[id].addr.send(totalFunds/10);
+        //Figure out which callback is it
+        if (post_cb_index[id]) { //Callback
+            Troll t = trolls[post_cb_index[id]];
+            string verification_url = strConcat("json(http://www.reddit.com/api/info.json?id=t1_", t.troll_post_id, ").data.children.0.data.body");
+            bytes32 post_cb_id = oraclize_query("URL", verification_url);
+        }
+        //If post callback then verify Score above threshold
+        //If verification callback then payout if verification is successful
+    }
+
+    function payReward(Troll t) internal {
+        if (t.reward > 0) {
+            t.reward = totalFunds/10;
+            totalFunds = totalFunds - t.reward;
+            t.addr.send(totalFunds/10);
         }
     }
-    
+
     //The method trolls need to call in order to claim the reward.
     //We give 10% of the current pool as the reward. This ensures that for a long time people will be able to claim rewards
-    function claimReward(string troll_post_id, string proof_comment_id) returns (string url) {
-        url = strConcat("json(http://www.reddit.com/by_id/t3_", troll_post_id, ".json).data.children.0.data.score");
-        bytes32 trollID = oraclize_query("URL", url);
-        trolls[trollID] = Troll({
+    function claimReward(string troll_post_id, string proof_comment_id) returns (string post_url) {
+        post_url = strConcat("json(http://www.reddit.com/by_id/t3_", troll_post_id, ".json).data.children.0.data.score");
+        bytes32 post_cb_id = oraclize_query("URL", post_url);
+        post_cb_index[post_cb_id] = troll_post_id;
+        trolls[troll_post_id] = Troll({
             addr: msg.sender,
             troll_post_id: troll_post_id,
             proof_comment_id: proof_comment_id,
             reward: 0
         });
     }
-} 
+}
