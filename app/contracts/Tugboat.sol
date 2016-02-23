e2d7461dd36e06e5b7bc0e84e36acdd675301818
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-02-22 23:18:32 -0500

    Adding Tugboat.sol

---
+++ app/contracts/Tugboat.sol
@@ -0,0 +1,23 @@
+import "dev.oraclize.it/api.sol";
+import "std";
+
+contract Tugboat is usingOraclize, mortal {
+    bool updateScheduled;
+    uint8 updateFrequency;
+    
+    function Tugboat() {
+        
+    }
+    
+    function scheduleUpdate() onlyowner {
+        
+    }
+    
+    function depositFunds() {
+        
+    }
+    
+    function claimReward() {
+        
+    }
+}
