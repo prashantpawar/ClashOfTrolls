e660c5ad46f31813fa372f28f12727455e946b7c
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-02-27 00:01:06 -0500

    Added initial test\n Added test task in package.json

---
+++ tests/ClashOfTrollsTest.sol
@@ -0,0 +1,10 @@
+import "contracts/ClashOfTrolls.sol";
+
+contract ClashOfTrollsTest {
+    uint FUNDS = 1010134;
+
+    function testDepositFunds() {
+        ClashOfTrolls c = new ClashOfTrolls();
+        /*c.depositFunds.value(FUNDS)();*/
+    }
+}
